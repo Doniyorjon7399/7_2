1cba06ea5f33ceb15fbe339869b257b571ea5d9d
Doniyorjon7399 <zokirovdoniyorjon2001@gmail.com>
2025-03-23 14:16:47 +0500

    7_2.

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -5,67 +5,42 @@ import {
   Get,
   HttpCode,
   Param,
-  Patch,
   Post,
   Put,
 } from '@nestjs/common';
 import { BlogDto } from './dto/blog.dto';
+import { BlogService } from './blog.service';
+
 @Controller('blog')
 export class BlogController {
-  blogs: BlogDto[];
-  constructor() {
-    this.blogs = [
-      {
-        id: 1,
-        title: 'nestjs',
-        excerpt: 'hello',
-        description: 'hello nestjs',
-      },
-      {
-        id: 2,
-        title: 'nestjs',
-        excerpt: 'hello',
-        description: 'hello nestjs',
-      },
-      {
-        id: 3,
-        title: 'nestjs',
-        excerpt: 'hello',
-        description: 'hello nestjs',
-      },
-    ];
-  }
+  constructor(private readonly blogService: BlogService) {}
   @HttpCode(200)
   @Get('all')
   async getAll() {
-    return this.blogs;
+    return this.blogService.getAllBlog();
   }
+
   @HttpCode(201)
   @Post('create')
   async create(@Body() dto: BlogDto) {
-    delete dto.id;
-    const data: BlogDto = {
-      id: new Date().getTime(),
-      ...dto,
-    };
-    return [...this.blogs, data];
+    return this.blogService.create(dto);
   }
+
   @HttpCode(200)
   @Get(':id')
   async getById(@Param('id') id: string) {
-    return this.blogs.find((itim) => itim.id == Number(id));
+    return this.blogService.getById(id);
   }
+
   @HttpCode(200)
   @Put(':id')
   async update(@Body() dto: BlogDto, @Param('id') id: string) {
-    let currentBlog = this.blogs.find((item) => item.id == Number(id));
-    currentBlog = dto;
-    return currentBlog;
+    return this.blogService.update(dto, id);
   }
+
   @HttpCode(201)
   @Delete(':id')
   async delete(@Param('id') id: string) {
-    const delleteUser = this.blogs.filter((item) => item.id !== Number(id));
-    return delleteUser;
+    return this.blogService.delete(id);
   }
 }
--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -1,4 +1,52 @@
 import { Injectable } from '@nestjs/common';
+import { BlogDto } from './dto/blog.dto';
 
 @Injectable()
-export class BlogService {}
+export class BlogService {
+  blogs: BlogDto[];
+  constructor() {
+    this.blogs = [
+      {
+        id: 1,
+        title: 'nestjs',
+        excerpt: 'hello',
+        description: 'hello nestjs',
+      },
+      {
+        id: 2,
+        title: 'nestjs',
+        excerpt: 'hello',
+        description: 'hello nestjs',
+      },
+      {
+        id: 3,
+        title: 'nestjs',
+        excerpt: 'hello',
+        description: 'hello nestjs',
+      },
+    ];
+  }
+  async getAllBlog() {
+    return this.blogs;
+  }
+  async create(dto: BlogDto) {
+    delete dto.id;
+    const data: BlogDto = {
+      id: new Date().getTime(),
+      ...dto,
+    };
+    return [...this.blogs, data];
+  }
+  async getById(id: string) {
+    return this.blogs.find((itim) => itim.id == Number(id));
+  }
+  async update(dto: BlogDto, id: string) {
+    let currentBlog = this.blogs.find((item) => item.id == Number(id));
+    currentBlog = dto;
+    return currentBlog;
+  }
+  async delete(id: string) {
+    const delleteUser = this.blogs.filter((item) => item.id !== Number(id));
+    return delleteUser;
+  }
+}
